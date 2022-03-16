93556c24b429e316c8754945d402fff21137ed26
Daniel Ungureanu <leinad.ungureanu@gmail.com>
2022-03-16 19:13:46 +0000

    created separate repo for oop_management project

---
+++ main_employee_app.py
@@ -0,0 +1,10 @@
+#! python3
+import choice_selection_oop
+
+
+if __name__ == '__main__':
+    print("This is the management_system.py")
+    while True:
+        choice_selection_oop.select_option()
+        break
+
